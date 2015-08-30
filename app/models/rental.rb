1230bb481f97d8dcecba5f2bc07c32bdfa771513
Justyna Rachowicz <mrowa44@gmail.com>
2015-08-30 17:09:12 +0200

    Add rental model validations

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,3 +1,32 @@
 class Rental < ActiveRecord::Base
   belongs_to :car
+
+  validates :starts_on, presence: true
+  validates :ends_on, presence: true
+  validate :cannot_start_in_the_past
+  validate :cannot_end_in_the_past
+  validate :cannot_end_before_it_starts
+  validates :starts_on, :ends_on, overlap: { message_title: 'This car', message_content: "is not available during this time" }
+
+  def cannot_start_in_the_past
+    if starts_on < Date.today
+      errors.add(:starts_on, "can't be in the past")
+    end
+  end
+
+  def cannot_end_in_the_past
+    if ends_on < Date.today
+      errors.add(:ends_on, "can't be in the past")
+    end
+  end
+
+  def cannot_end_before_it_starts
+    if ends_on < starts_on
+      errors.add(:base, "Rental can't end before it's started")
+    end
+  end
+
+  def days
+    (ends_on - starts_on).to_i
+  end
 end
