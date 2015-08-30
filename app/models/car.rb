18d940e9cba5da4a0d9f873dfae9be88a59ffa46
Justyna Rachowicz <mrowa44@gmail.com>
2015-08-30 16:02:09 +0200

    Add car validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,6 @@
 class Car < ActiveRecord::Base
   has_many :rentals
+
+  validates :name, presence: true
+  validates :kind, inclusion: { in: ["A", "B", "C"],  message: 'has to be one of A, B, C' }
 end
